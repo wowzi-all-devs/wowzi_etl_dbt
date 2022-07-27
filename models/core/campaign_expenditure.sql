ebf998b8760463bf9fb938b0814bd2ace7de08da
mateo2181 <mateomerlo@gmail.com>
2022-07-27 14:40:46 +0200

    Added core model Campaign Expenditure and mart model Influencer Campaign Facts.

---
+++ models/core/campaign_expenditure.sql
@@ -0,0 +1,17 @@
+SELECT
+    campaign_name,
+    campaign_id,
+    campaign_start_date,
+    campaign_end_date,
+    campaign_bonus_amount,
+    campaign_budget,
+    budget_spent,
+    agency_commission,
+    influencers_invited_count,
+    influencers_accepted_count,
+    merchant_id,
+    merchant_name,
+    company_name,
+    currency,
+    canceled
+FROM {{ source('dbt_caleb', 'campaign_expenditure') }}
---
+++ models/marts/influencer_success/influencer_campaign_facts.sql
@@ -0,0 +1,44 @@
+WITH
+  task_data AS(
+  SELECT
+    campaign_id,
+    COUNT(id) AS total_tasks,
+    COUNTIF(first_verification_status="APPROVED") quality_verified_tasks,
+    COUNTIF(second_verification_status="APPROVED") problematic_verified_tasks,
+    COUNTIF(third_verification_status="APPROVED") time_verified_tasks,
+  FROM
+    {{ ref('influencer_tasks') }}
+  GROUP BY
+    1 
+)
+SELECT
+  c.campaign_id,
+  c.name as campaign_name,
+  c.campaign_creation_date,
+  c.start_date,
+  c.end_date,
+  c.currency,
+  c.budget,
+  ce.budget_spent,
+  c.bonus_amount,
+  c.canceled,
+  c.canceled_timestamp,
+  ce.influencers_invited_count,
+  ce.influencers_accepted_count,
+  ce.company_name,
+  task_data.total_tasks,
+  task_data.quality_verified_tasks,
+  task_data.problematic_verified_tasks,
+  task_data.time_verified_tasks
+FROM
+  {{ ref('campaigns') }} c
+LEFT JOIN
+  task_data
+ON
+  c.campaign_id=task_data.campaign_id
+LEFT JOIN
+  {{ ref('campaign_expenditure') }} ce
+ON
+  c.campaign_id=CAST(ce.campaign_id AS integer)
+ORDER BY
+  c.campaign_id DESC
