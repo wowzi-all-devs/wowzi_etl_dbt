ffd4fa981fc14f8af4660da9c2a6081b36c96a36
mateo2181 <mateomerlo@gmail.com>
2022-07-27 17:42:04 +0200

    Added model Influencer Task Facts.

---
+++ models/marts/influencer_success/influencer_task_facts.sql
@@ -0,0 +1,28 @@
+SELECT
+  inft.influencer_id,
+  c.company_id,
+  inft.campaign_id,
+  ct.channel,
+  submission_link,
+  submission_status,
+  first_verification_status,
+  second_verification_status,
+  third_verification_status,
+  payment_amount_list,
+  top_post,
+  inft.creation_time AS task_creation_time,
+  country
+FROM
+  {{ ref('influencer_tasks') }} inft
+LEFT JOIN
+  {{ ref('influencers') }}
+USING
+  (influencer_id)
+LEFT JOIN
+  {{ ref('campaigns') }} c
+ON
+  inft.campaign_id=c.id
+LEFT JOIN
+  {{ ref('campaign_tasks') }} ct
+ON
+  inft.campaign_task_id=ct.id
