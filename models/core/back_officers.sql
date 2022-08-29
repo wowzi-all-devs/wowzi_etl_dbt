113e300b1c6f1feadd4399f3a4c0cc3b25817b78
mateo2181 <mateomerlo@gmail.com>
2022-08-29 10:32:35 +0200

    Added customer support data mart.

---
+++ models/core/back_officers.sql
@@ -0,0 +1,6 @@
+select 
+    id as backofficer_id,
+    concat(first_name, ' ', last_name) as backofficer_name,
+    email,
+    role
+FROM {{ source('core', 'backofficers') }}
---
+++ models/marts/customer_support/top_post_verification_last_month_by_backofficer_by_day.sql
@@ -0,0 +1,40 @@
+{{ config(tags=["customer_support"]) }}
+
+
+WITH approvals AS (
+  SELECT id,
+    date(creation_time) AS date_creation,
+    submission_status,
+    top_post,
+    date(top_post_selected_time) AS top_post_selected_time,
+    submission_link,
+    cast(first_verification_feedback_backofficer_id AS int) AS backofficer_id,
+  FROM {{ ref('influencer_tasks') }}
+  WHERE top_post IS TRUE
+),
+bo AS (
+  SELECT
+    backofficer_id,
+    backofficer_name,
+    email,
+    role
+  FROM {{ ref('back_officers') }}
+),
+joined_tables AS (
+  SELECT *
+  FROM approvals
+  LEFT JOIN bo USING(backofficer_id)
+)
+SELECT
+  {{ dbt_utils.surrogate_key(['top_post_selected_time', 'backofficer_id']) }} as primary_key, 
+  top_post_selected_time,
+  backofficer_id,
+  backofficer_name,
+  count(top_post) AS num_top_posts_verification
+FROM joined_tables
+WHERE 
+  backofficer_id IS NOT NULL
+  AND date_diff(date(current_timestamp()),date(top_post_selected_time), MONTH) <=1
+  AND backofficer_id IN (select backofficer_id from bo)
+GROUP BY 2,3,4
+ORDER BY 2 DESC
