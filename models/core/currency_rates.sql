27f524f3a02daa6580cdd585b432e897b07831f2
mateo2181 <mateomerlo@gmail.com>
2022-09-09 10:10:52 +0200

    Fixed currency_rates model to avoid duplication.

--- models/core/currency_rates.sql
+++ models/core/currency_rates.sql
@@ -1,6 +1,21 @@
+WITH unique_currency_by_date AS (
+SELECT
+    date,
+    base,
+    currency,
+    currency_rate,
+    ROW_NUMBER() OVER(
+        PARTITION BY date, base, currency
+        ORDER BY date desc
+    ) as row_number,
+FROM {{ source('rates', 'currency_rates_test') }}
+)
 SELECT
     date,
     base,
     currency,
     currency_rate
-FROM {{ source('rates', 'currency_rates_test') }}
+FROM 
+    unique_currency_by_date
+WHERE 
+    row_number = 1
