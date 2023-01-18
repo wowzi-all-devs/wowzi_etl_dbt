5cfe7be1de76b5af9371f7d8434a4c726e42f9b9
Caleb <calebe@wowzi.co>
2023-01-18 08:59:00 +0000

    Squashed commit of the following
    commit 523b931c 
    Author: kanataki-zwei <115462648+kanataki-zwei@users.noreply.github.com> 
    Date: Mon Jan 16 2023 16:04:42 GMT+0300 (East Africa Time) 
    
        first commit. created influencer referrals table for referrals dashboard
    
    
    commit 055c948e 
    Author: kanataki-zwei <115462648+kanataki-zwei@users.noreply.github.com> 
    Date: Mon Jan 16 2023 16:02:35 GMT+0300 (East Africa Time) 
    
        added new columns for payouts dashboard

---
+++ models/marts/core/influencer_referrals.sql
@@ -0,0 +1,38 @@
+WITH payments AS (
+SELECT 
+    influencer_id,
+    sum(amount_usd) as amount_usd
+FROM {{ ref('influencer_payouts') }}
+    WHERE lower(payment_status) in ('successful', 'manual', 'new')
+    GROUP BY influencer_id
+),
+
+influencer_referrals AS (
+SELECT
+    ref.influencer_id,
+    (CASE WHEN inf.gender IS NULL THEN 'GENDER NOT SET'
+    ELSE inf.gender END) AS gender,
+    inf.job_eligibility,
+    (CASE WHEN inf.country IS NULL THEN 'COUNTRY NOT SET'
+    ELSE inf.country END) AS country,
+    inf.offered_jobs AS total_offered_jobs,
+    (CASE WHEN inf.offered_jobs > 0 then 'YES'
+    ELSE 'NO' END) AS inf_offered_job,
+    inf.total_accepted AS total_accepted_jobs,
+    (CASE WHEN inf.total_accepted > 0 then 'YES'
+    ELSE 'NO' END) AS inf_accepted_job,
+    p.amount_usd,
+    (CASE WHEN p.amount_usd > 0 then 'YES'
+    ELSE 'NO' END) AS inf_completed_job,
+    inf.job_activity,
+    ref.referred_by_influencer_id as ambassador_influencer_id,
+    inf2.job_eligibility as ambassador_job_eligibility,
+    inf2.country as ambassador_country,
+    ref.creation_time
+FROM {{ ref('postgres_stg__influencer_referral_influencer') }} ref
+LEFT JOIN {{ ref('influencer_facts') }} inf on ref.influencer_id = inf.influencer_id
+LEFT JOIN {{ ref('influencer_facts') }} inf2 on ref.influencer_id = inf2.influencer_id
+LEFT JOIN payments p on ref.influencer_id = p.influencer_id
+)
+
+SELECT * FROM influencer_referrals
--- models/marts/finance/influencer_payouts.sql
+++ models/marts/finance/influencer_payouts.sql
@@ -1,10 +1,16 @@
 {{ config(tags=["finance"]) }}
 
-WITH dims_payments AS (
+WITH payments AS (
 SELECT      
     inf_transfers.id as transfer_id,
     inf_transfers.influencer_id,
     inf.mobile_number,
+    inf.gender,
+    inf.influencer_type_TWITTER,
+    inf.influencer_type_FACEBOOK,
+    inf.influencer_type_LINKEDIN,
+    inf.influencer_type_TIKTOK,
+    inf.influencer_type_INSTAGRAM,
     inf_tasks.campaign_id,
     companies.company_name,
     adv.advertiser_id,
@@ -12,7 +18,17 @@ SELECT
     adv.last_name as advertiser_lname,
     campaigns.campaign_name,
     inf_transfers.task_id,
+    (CASE WHEN lower(inf_tasks.submission_link) LIKE '%twitter%' THEN 'TWITTER'
+    WHEN lower(inf_tasks.submission_link) LIKE '%t.co%' THEN 'TWITTER'
+    WHEN lower(inf_tasks.submission_link) LIKE '%facebook%' THEN 'FACEBOOK'
+    WHEN lower(inf_tasks.submission_link) LIKE '%linkedin%' THEN 'LINKEDIN'
+    WHEN lower(inf_tasks.submission_link) LIKE '%tiktok%' THEN 'TIKTOK'
+    WHEN lower(inf_tasks.submission_link) LIKE '%instagram%' THEN 'INSTAGRAM'
+    WHEN lower(inf_tasks.submission_link) LIKE '%youtube%' THEN 'YOUTUBE'
+    WHEN lower(inf_tasks.submission_link) IS NULL THEN 'NOT-SUBMITTED'
+    end) as social_media_channel,
     inf_transfers.amount,
+    inf_transfers.amount/rates.currency_rate as amount_usd,
     inf_transfers.status as payment_status,
     date(inf_transfers.date_created) as payment_date,
     inf_transfers.currency as currency,
@@ -22,24 +38,42 @@ SELECT
     inf_transfers.narration,
     inf_transfers.reference
 FROM {{ ref('postgres_stg__influencer_transfers') }} inf_transfers
-   LEFT JOIN {{ ref('postgres_stg__influencers') }} inf USING (influencer_id)
+   LEFT JOIN {{ ref('influencer_facts') }} inf USING (influencer_id)
    LEFT JOIN {{ ref('int_bank_details') }} bd USING (influencer_id)
    LEFT JOIN {{ ref('postgres_stg__influencer_tasks') }} inf_tasks ON inf_tasks.task_id = inf_transfers.task_id
    LEFT JOIN {{ ref('postgres_stg__campaigns') }} campaigns ON campaigns.campaign_id=inf_tasks.campaign_id
    LEFT join {{ ref('postgres_stg__merchants') }} adv on adv.advertiser_id=campaigns.merchant_id
    LEFT join {{ ref('postgres_stg__companies') }} companies on companies.company_id = campaigns.company_id
-)
+   LEFT JOIN {{ ref('int_currency_rates') }} rates ON cast(inf_transfers.date_created as date) = cast(rates.date as date)
+   AND upper(inf_transfers.currency) = upper(rates.currency)
+),
+
+dims_payments as (
 SELECT
     transfer_id,
     influencer_id,
     mobile_number,
+    gender,
     advertiser_id,
     campaign_id,
     task_id,
     campaign_name,
+    social_media_channel,
+    (CASE WHEN social_media_channel = 'TWITTER'
+    THEN influencer_type_TWITTER
+    WHEN social_media_channel = 'FACEBOOK'
+    THEN influencer_type_FACEBOOK
+    WHEN social_media_channel = 'LINKEDIN'
+    THEN influencer_type_LINKEDIN
+    WHEN social_media_channel = 'TIKTOK'
+    THEN influencer_type_TIKTOK
+    WHEN social_media_channel = 'INSTAGRAM'
+    THEN influencer_type_INSTAGRAM
+    END) as task_influencer_type,
     payment_date,
     country,
     amount,
+    amount_usd,
     currency,
     payment_status,
     bank_name,
@@ -47,4 +81,6 @@ SELECT
     reference,
     narration
 FROM 
-    dims_payments
+    payments)
+    
+select * from dims_payments
