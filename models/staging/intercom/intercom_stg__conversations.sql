e9c4e1e7f4bd2a9ed89f6444bed42deb76488265
Caleb Ejakait <calebe@wowzi.co>
2023-01-24 13:37:24 +0300

    cast string values to int

--- models/staging/intercom/intercom_stg__conversations.sql
+++ models/staging/intercom/intercom_stg__conversations.sql
@@ -2,13 +2,13 @@
 WITH
   conversations AS (
   SELECT
-    id AS conversation_id,
+    cast(id as int) AS conversation_id,
     JSON_EXTRACT(tags, '$.tags[0].name') AS tag,
-    JSON_EXTRACT_SCALAR(teammates, '$.admins[0].id') AS admin_id,
-    JSON_EXTRACT_SCALAR(statistics, '$.time_to_admin_reply') AS time_to_admin_reply,
-    JSON_EXTRACT_SCALAR(statistics, '$.count_conversation_parts') AS count_conversation_parts,
-    JSON_EXTRACT_SCALAR(statistics, '$.median_time_to_reply') AS median_time_to_reply,
-    JSON_EXTRACT_SCALAR(conversation_rating, '$.rating') AS conversation_rating,
+    cast(JSON_EXTRACT_SCALAR(teammates, '$.admins[0].id') as int )AS admin_id,
+    cast(JSON_EXTRACT_SCALAR(statistics, '$.time_to_admin_reply') as int )AS time_to_admin_reply,
+    cast(JSON_EXTRACT_SCALAR(statistics, '$.count_conversation_parts')as int)AS count_conversation_parts,
+    cast(JSON_EXTRACT_SCALAR(statistics, '$.median_time_to_reply')as int) AS median_time_to_reply,
+    cast(JSON_EXTRACT_SCALAR(conversation_rating, '$.rating') as int) AS conversation_rating,
     JSON_EXTRACT(conversation_rating, '$.remark') AS conversation_rating_remark,
     TIMESTAMP_SECONDS(created_at) AS created_at
   FROM
@@ -22,4 +22,4 @@ FROM
 LEFT JOIN
   {{ source('staging', 'admins')}} admins
 ON
-  admins.id=conversations.admin_id
+  cast(admins.id as int)=conversations.admin_id
