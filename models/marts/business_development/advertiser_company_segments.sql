d29de1ec6281be59df706c3a7569866209bf44fe
kanataki-zwei <115462648+kanataki-zwei@users.noreply.github.com>
2023-06-23 11:00:06 +0300

    remodelled the segmentation logic for advertisers

--- models/marts/business_development/advertiser_company_segments.sql
+++ models/marts/business_development/advertiser_company_segments.sql
@@ -1,159 +1,177 @@
-WITH first_campaign AS 
+WITh merchants AS 
 (SELECT  
-  merchant_id, merchant_name, company_name, 
-  min(date(campaign_start_date)) as first_campaign_start_date
-FROM {{ ref('campaign_expenditure') }} 
-  where lower(campaign_name) not like '%test%'
-  and budget_spent > 0
-  and merchant_id <> 7
-  and lower(company_name) not like '%wowzi%'
-  group by merchant_id, merchant_name, company_name),
-
-campaign_months AS
-(select 
-  merchant_id,company_name, count(distinct months) as no_of_months_with_campaigns_started
+  distinct merchant_id, merchant_name, company_name
+FROM `bi-staging-1-309112.wowzi_dbt_prod.campaign_expenditure` 
+  where merchant_id in  
+  (select
+  first_merchant
 from
 (SELECT 
-  merchant_id, merchant_name, company_name,
-  DATE_TRUNC(date(campaign_start_date), month) as months
-FROM {{ ref('campaign_expenditure') }} 
+  company_name, 
+  min(merchant_id) as first_merchant
+FROM `bi-staging-1-309112.wowzi_dbt_prod.campaign_expenditure` 
   where lower(campaign_name) not like '%test%'
   and budget_spent > 0
   and merchant_id <> 7
-  and lower(company_name) not like '%wowzi%') a
-  group by merchant_id, company_name),
+  and merchant_id <> 8
+  and lower(company_name) not like '%wowzi%'
+  and company_name <> 'Advertiser Demo Account '
+  group by company_name))),
 
-last_campaign AS
-(SELECT  
-  merchant_id, merchant_name, company_name, 
-  max(date(campaign_start_date)) as campaign_start_date
-FROM {{ ref('campaign_expenditure') }} 
+first_campaign AS 
+(SELECT 
+  company_name, 
+  min(date(campaign_start_date)) as first_campaign_start_date 
+FROM `bi-staging-1-309112.wowzi_dbt_prod.campaign_expenditure` 
   where lower(campaign_name) not like '%test%'
   and budget_spent > 0
   and merchant_id <> 7
   and lower(company_name) not like '%wowzi%'
-  group by merchant_id, merchant_name, company_name),
+  and company_name <> 'Advertiser Demo Account '
+  group by company_name),
 
-campaign_budget AS
+last_campaign AS 
 (SELECT 
-  merchant_id, merchant_name, company_name,
+  company_name, 
+  max(date(campaign_start_date)) as last_campaign_start_date 
+FROM `bi-staging-1-309112.wowzi_dbt_prod.campaign_expenditure` 
+  where lower(campaign_name) not like '%test%'
+  and budget_spent > 0
+  and merchant_id <> 7
+  and merchant_id <> 8
+  and lower(company_name) not like '%wowzi%'
+  and company_name <> 'Advertiser Demo Account '
+  group by company_name),
+
+campaign_months AS 
+(SELECT 
+  a.company_name,
+  count(distinct a.months) as no_of_months_with_campaigns_started
+from
+(SELECT 
+  company_name, 
+  DATE_TRUNC(date(campaign_start_date), month) as months
+FROM `bi-staging-1-309112.wowzi_dbt_prod.campaign_expenditure` 
+  where lower(campaign_name) not like '%test%'
+  and budget_spent > 0
+  and merchant_id <> 7
+  and lower(company_name) not like '%wowzi%'
+  and company_name <> 'Advertiser Demo Account ') a
+  group by a.company_name),
+
+campaign_budgets AS 
+(select 
+  company_name,
   sum(budget_spent) as budget_spent
-FROM
-(SELECT 
-  merchant_id, merchant_name, company_name, date(campaign_start_date) as campaign_start_date,
+from
+(SELECT  
+  company_name, 
+  date(campaign_start_date) as campaign_start_date,
   ce.currency, budget_spent/cr.currency_rate as budget_spent
-FROM {{ ref('campaign_expenditure') }}  ce
-LEFT JOIN {{ ref('int_currency_rates') }} cr on upper(ce.currency) = upper(cr.currency)
+FROM `bi-staging-1-309112.wowzi_dbt_prod.campaign_expenditure` ce 
+LEFT JOIN `bi-staging-1-309112.wowzi_dbt_prod.currency_rates` cr ON upper(ce.currency) = upper(cr.currency)
 and date(campaign_start_date) = date(cr.date)
   where lower(campaign_name) not like '%test%'
   and budget_spent > 0
   and merchant_id <> 7
-  and lower(company_name) not like '%wowzi%') budgets_usd
-  group by merchant_id, merchant_name, company_name),
+  and lower(company_name) not like '%wowzi%'
+  and company_name <> 'Advertiser Demo Account ') budget
+  group by company_name),
 
 campaign_rm AS
 (SELECT 
-   c.merchant_id, c.merchant_name, c.company_name,
-   campaign_start_date as latest_campaign_start_date,
-   DATE_DIFF(current_date, c.campaign_start_date, day) as R,
-   cb.budget_spent as M, cm.no_of_months_with_campaigns_started as F
-  FROM last_campaign c 
-  LEFT JOIN campaign_budget cb ON c.merchant_id = cb.merchant_id
-  AND c.company_name = cb.company_name
-  LEFT JOIN campaign_months cm ON c.merchant_id = cm.merchant_id
-  AND c.company_name = cm.company_name),  
+  m.merchant_id, m.merchant_name,
+  fc.company_name,
+  fc.first_campaign_start_date,
+  lc.last_campaign_start_date AS latest_campaign_start_date,
+  DATE_DIFF(current_date, lc.last_campaign_start_date, day) AS R,
+  cm.no_of_months_with_campaigns_started AS F,
+  cb.budget_spent AS M
+FROM first_campaign fc 
+LEFT JOIN merchants m ON fc.company_name = m.company_name
+LEFT JOIN last_campaign lc ON fc.company_name = lc.company_name
+LEFT JOIN campaign_months cm ON fc.company_name = cm.company_name
+LEFT JOIN campaign_budgets cb ON fc.company_name = cb.company_name),
+
+max_scores AS
+(SELECT 
+  max(R) AS max_R,
+  max(F) AS max_F,
+  max(M) AS max_M
+FROM campaign_rm),
 
 campaign_scores AS
-(SELECT 
-    crm.merchant_id,
-    crm.merchant_name,
-    crm.company_name,
-    crm.latest_campaign_start_date,
-    crm.R,
-    crm.F,
-    crm.M
-    --NTILE(10) OVER (ORDER BY R DESC) as R_S,
-    --NTILE(10) OVER (ORDER BY F ASC) as F_S,
-    --NTILE(10) OVER (ORDER BY M ASC) as M_S
-FROM campaign_rm crm),
+(SELECT
+  crm.merchant_id, 
+  crm.merchant_name,
+  crm.company_name,
+  crm.first_campaign_start_date,
+  crm.latest_campaign_start_date,
+  crm.R,
+  crm.F,
+  crm.M,
+  10 - ((10/ms.max_R)*crm.R) AS R_S,
+  (10/ms.max_F)*crm.F AS F_S,
+  (10/ms.max_M)*crm.M AS M_S,
+  (10 - ((10/ms.max_R)*crm.R))+((10/ms.max_F)*crm.F)+((10/ms.max_M)*crm.M) AS T
+FROM campaign_rm crm
+LEFT JOIN max_scores ms ON ms.max_R = ms.max_R),
 
-max_initial_scores AS 
-(SELECT 
-  max(R) AS R,
-  max(F) AS F,
-  max(M) AS M
-FROm campaign_scores),
+max_main_score AS 
+(SELECT
+  max(T) AS max_T
+FROM campaign_scores),
 
-campaign_scores_2 AS
+campaign_final_scores AS
 (SELECT 
-    c.merchant_id,
-    c.merchant_name,
-    c.company_name,
-    c.latest_campaign_start_date,
-    c.R,
-    c.F,
-    c.M,
-    10 - ((10/m.R)*c.R) AS R_S,
-    (10/m.F)*c.F AS F_S,
-    (10/m.M)*c.M AS M_S,
-    (10 - ((10/m.R)*c.R))+((10/m.F)*c.F)+((10/m.M)*c.M) AS T
-FROM campaign_scores c 
-LEFT JOIN max_initial_scores m ON m.R = m.R),
-
-max_main_score AS
-(SELECT 
-  max(T) AS T
-FROM campaign_scores_2),
-
-campaign_final_scores AS 
-(SELECT 
-  c.merchant_id,
-  c.merchant_name,
-  c.company_name,
-  c.latest_campaign_start_date,
-  c.R,
-  c.F,
-  c.M,
-  c.R_S,
-  c.F_S,
-  c.M_S,
-  c.T,
-  (10/m.T)*c.T AS T_S
-FROM campaign_scores_2 c
-LEFT JOIN max_main_score m on m.T = m.T)
+  cs.merchant_id, 
+  cs.merchant_name,
+  cs.company_name,
+  cs.first_campaign_start_date,
+  cs.latest_campaign_start_date,
+  cs.R,
+  cs.F,
+  cs.M,
+  cs.R_S,
+  cs.F_S,
+  cs.M_S,
+  cs.T,
+  (10/ms.max_T)*cs.T AS T_S
+FROM campaign_scores cs
+LEFT JOIN max_main_score ms ON ms.max_T = ms.max_T)
 
 SELECT 
-    cfs.merchant_id,
-    cfs.merchant_name,
-    cfs.company_name,
-    fc.first_campaign_start_date,
-    cfs.latest_campaign_start_date,
-    cfs.R as days_since_last_campaign,
-    cfs.F as no_of_months_with_campaigns_started,
-    cfs.M as total_budget_spend_usd,
-    cfs.R_S as recency_score,
-    cfs.F_S as frequency_score,
-    cfs.M_S as budget_spend_score,
-    cfs.T_S as total_score,
-    (CASE 
-        WHEN cfs.T_S >= 8
-        THEN 'Super Advertiser'
-        WHEN cfs.T_S >= 6 AND cfs.T_S < 8
-        THEN 'Dependable Advertiser'
-        WHEN cfs.T_S >= 4 AND  cfs.T_S <= 6
-        THEN 'Potential Dependable Advertiser'
-        WHEN cfs.T_S >=3 AND cfs.T_S < 4
-        AND cfs.R < 90
-        THEN 'Need Attention'
-        WHEN cfs.T_S >= 3 AND cfs.T_S < 4
-        AND cfs.R >= 90
-        THEN 'At Risk/Churned'
-        WHEN cfs.R >= 90
-        THEN 'At Risk/Churned'
-        WHEN cfs.T_S <=3
-        THEN 'At Risk/Churned'
-    END) as company_segment,
-    (CASE 
+  cfs.merchant_id, 
+  cfs.merchant_name,
+  cfs.company_name,
+  cfs.first_campaign_start_date,
+  cfs.latest_campaign_start_date,
+  cfs.R AS days_since_last_campaign,
+  cfs.F AS no_of_months_with_campaigns_started,
+  cfs.M AS total_budget_spend_usd,
+  cfs.R_S AS recency_score,
+  cfs.F_S AS frequency_score,
+  cfs.M_S AS budget_spend_score,
+  cfs.T_S AS total_score,
+  (CASE 
+      WHEN cfs.T_S >= 8
+      THEN 'Super Advertiser'
+      WHEN cfs.T_S >= 6 AND cfs.T_S < 8
+      THEN 'Dependable Advertiser'
+      WHEN cfs.T_S >= 4 AND  cfs.T_S <= 6
+      THEN 'Potential Dependable Advertiser'
+      WHEN cfs.T_S >=3 AND cfs.T_S < 4
+      AND cfs.R < 90
+      THEN 'Need Attention'
+      WHEN cfs.T_S >= 3 AND cfs.T_S < 4
+      AND cfs.R >= 90
+      THEN 'At Risk/Churned'
+      WHEN cfs.R >= 90
+      THEN 'At Risk/Churned'
+      WHEN cfs.T_S <=3
+      THEN 'At Risk/Churned'
+  END) as company_segment,
+  (CASE 
     WHEN cfs.R < 30 
     THEN 'Less than 30 Days'
     WHEN cfs.R >= 30 
@@ -182,4 +200,3 @@ SELECT
     THEN 'Less than $25,000'
     END) as budget_spend_brackets
 FROM campaign_final_scores cfs
-LEFT JOIN first_campaign fc on cfs.merchant_id = fc.merchant_id
