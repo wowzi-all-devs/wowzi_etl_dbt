37d3d8a82315865196f240b7813fdf495095fac6
Caleb Ejakait <calebe@wowzi.co>
2023-06-27 16:39:58 +0300

    update macro for tiktok submission links

--- macros/channel_from_submission_link.sql
+++ macros/channel_from_submission_link.sql
@@ -4,6 +4,6 @@ case
     when submission_link like "%twitter%" or submission_link like "%t.co%" then 'Twitter'
     when submission_link like "%facebook%" then 'Facebook'
     when submission_link like "%instagram%" then "Instagram"
-    when submission_link like "%titok%" then "Tiktok"
+    when submission_link like "%tiktok%" then "Tiktok"
 end
 {% endmacro %}
