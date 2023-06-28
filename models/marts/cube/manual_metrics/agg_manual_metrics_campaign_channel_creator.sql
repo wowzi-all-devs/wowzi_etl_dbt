e921490dba81372a7abc3e370fbc8c5b1d0f23b4
mateo2181 <mateomerlo@gmail.com>
2023-06-28 14:29:25 +0200

    Updated agg_manual_metrics_campaign_channel_creator model.

---
+++ models/marts/cube/manual_metrics/agg_manual_metrics_campaign_channel_creator.sql
@@ -0,0 +1,17 @@
+{{ config(tags=["cube"]) }}
+
+select
+    campaign_id,
+    channel,
+    influencer_id,
+    task_id,
+    submission_link,
+    views as total_views,
+    shares as total_shares,
+    likes as total_likes,
+    comments as total_comments,
+    (views + shares + likes + comments) as total_engagement,
+    followers_count as follower_count,
+    updated_at
+
+from {{ ref('stg__manual_metrics') }}
