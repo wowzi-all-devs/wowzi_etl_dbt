d618808713d5a8c47d22753c9f4e73f04cf2895b
kanataki-zwei <115462648+kanataki-zwei@users.noreply.github.com>
2023-09-14 10:42:19 +0300

    added group tag

--- models/staging/intercom/intercom_stg__conversations.sql
+++ models/staging/intercom/intercom_stg__conversations.sql
@@ -3,6 +3,7 @@ WITH
   conversations AS (
   SELECT
     cast(id as int) AS conversation_id,
+    regexp_extract(REPLACE(JSON_EXTRACT(tags, '$.tags[0].name'), '"', ''), r'^[^:]*') as group_tag,
     JSON_EXTRACT(tags, '$.tags[0].name') AS tag,
     cast(JSON_EXTRACT_SCALAR(teammates, '$.admins[0].id') as int )AS admin_id,
     cast(JSON_EXTRACT_SCALAR(statistics, '$.time_to_admin_reply') as int )AS time_to_admin_reply,
