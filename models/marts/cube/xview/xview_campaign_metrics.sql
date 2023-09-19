927754ce4a09e55cd3e641dee8b27564abae2117
Caleb Ejakait <calebe@wowzi.co>
2023-09-19 15:21:59 +0300

    xview update

--- models/marts/cube/xview/xview_campaign_metrics.sql
+++ models/marts/cube/xview/xview_campaign_metrics.sql
@@ -1,35 +1,76 @@
-{{ config(tags=["cube"]) }}
+{{ config(
+  tags = ["cube"]
+) }}
 
-WITH
-  base_x_view AS (
+WITH base_x_view AS (
+
   SELECT
     cf.company_id,
-    c.company_name,
+    C.company_name,
     mmc.campaign_id,
     it.country,
-    sum(payment_amount_list) as amount_spent,
-    MAX(cf.budget) AS campaign_budget,
+    (
+      SELECT
+        country
+      FROM
+        {{ ref('postgres_stg__cluster_countries')}} clc
+      WHERE
+        clc.cluster_id = cc.cluster_id
+        AND clc.country = it.country
+    ) AS cluster_country,
+    cf.currency,
+    SUM(payment_amount_list) AS amount_spent,
+    MAX(
+      cf.budget
+    ) AS campaign_budget,
     SUM(total_follower_count) AS potential_reach,
     SUM(total_engagement) AS total_engagement,
     SUM(total_influencers) AS total_influencers,
   FROM
-    {{ref('agg_manual_metric_campaign_channel')}} mmc
-  left  JOIN
-    {{ ref('postgres_stg__campaigns') }}cf
-  ON
-    cf.campaign_id=mmc.campaign_id
-
-   left join {{ref('postgres_stg__companies')}} c on c.company_id=cf.company_id
-   left join {{ ref('influencer_task_facts')}} it on it.campaign_id=cf.campaign_id
-   where third_verification_status="APPROVED"  
+    {{ ref('agg_manual_metric_campaign_channel') }}
+    mmc
+    LEFT JOIN {{ ref('postgres_stg__campaigns') }}
+    cf
+    ON cf.campaign_id = mmc.campaign_id
+    LEFT JOIN {{ ref('postgres_stg__companies') }} C
+    ON C.company_id = cf.company_id
+    LEFT JOIN {{ ref('influencer_task_facts') }}
+    it
+    ON it.campaign_id = cf.campaign_id
+    LEFT JOIN {{ref('postgres_stg__cluster_companies')}} cc
+    ON cf.company_id = cc.company_id
+  WHERE
+    third_verification_status = "APPROVED"
   GROUP BY
     1,
-    2,3,4),
-  final_x_view AS (
+    2,
+    3,
+    4,
+    5,
+    6
+),
+final_x_view AS (
   SELECT
     base_x_view.*,
-    ROUND((amount_spent/total_engagement),2) AS cost_per_engagement
+    ROUND((amount_spent / total_engagement), 2) AS cost_per_engagement
   FROM
-    base_x_view)
-
-  select * from final_x_view
+    base_x_view
+)
+SELECT
+  final_x_view.*,
+  ROUND(
+    amount_spent / currency_rate,
+    2
+  ) AS dollar_amount_spent,
+  ROUND(
+    campaign_budget / currency_rate,
+    2
+  ) AS dollar_campaign_budget,
+  ROUND(
+    final_x_view.cost_per_engagement / currency_rate,
+    2
+  ) AS dollar_cpe
+FROM
+  final_x_view
+  LEFT JOIN {{ ref('int_currency_rates') }} cr
+  ON cr.currency = final_x_view.currency
--- models/marts/cube/xview/xview_cluster_metrics.sql
+++ models/marts/cube/xview/xview_cluster_metrics.sql
@@ -1,21 +1,30 @@
-{{ config(tags=["cube"]) }}
+{{ config(
+  tags = ["cube"]
+) }}
 
 SELECT
   cluster_id,
   cluster_name,
-  COUNT(DISTINCT campaign_id) AS total_campaigns,
+  cluster_country,
+  COUNT(
+    DISTINCT campaign_id
+  ) AS total_campaigns,
   SUM(campaign_budget) AS total_campaign_budget,
   SUM(amount_spent) AS total_spend,
   SUM(total_influencers) AS total_influencers,
   SUM(total_engagement) AS total_engagements,
   SUM(potential_reach) AS total_potential_reach,
-  ROUND(AVG(cost_per_engagement),0) AS avg_cpe
+  ROUND(AVG(cost_per_engagement), 0) AS avg_cpe
 FROM
-  {{ ref('xview_campaign_metrics')}} xm
-LEFT JOIN
-  {{ ref('postgres_stg__cluster_companies')}} cc
-ON
-  cc.company_id=xm.company_id
-left join {{ ref('postgres_stg__clusters')}} cl using(cluster_id)
+  {{ ref('xview_campaign_metrics') }}
+  xm
+  LEFT JOIN {{ ref('postgres_stg__cluster_companies') }}
+  cc
+  ON cc.company_id = xm.company_id
+  LEFT JOIN {{ ref('postgres_stg__clusters') }}
+  cl USING(cluster_id)
+WHERE
+  cluster_country IS NOT NULL
 GROUP BY
-  1,2
+  1,
+  2,3
