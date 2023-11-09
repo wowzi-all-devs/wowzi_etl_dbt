10d5be94580edd2321c9da11d904b2e802a68b4e
kanataki-zwei <115462648+kanataki-zwei@users.noreply.github.com>
2023-11-09 11:14:23 +0300

    adding models for clevertap events

---
+++ models/marts/core/clevertap_events.sql
@@ -0,0 +1,74 @@
+WITH app_installs AS 
+(SELECT 
+  event_datetime,
+  eventName,
+  profile_id,
+  profile_name,
+  profile_email,
+  profile_all_identities,
+  profile_platform,
+  push_token,
+  device_make,
+  device_model,
+  device_os_version,
+  app_version,
+  login_type,
+  login_session_id
+FROM {{ ref('clevertap_stg_events') }} 
+  where eventName = 'App Installed'),
+
+logins AS 
+(SELECT 
+  event_datetime,
+  eventName,
+  profile_id,
+  profile_name,
+  profile_email,
+  profile_all_identities,
+  profile_platform,
+  push_token,
+  device_make,
+  device_model,
+  device_os_version,
+  app_version,
+  login_type,
+  login_session_id,
+  row_number() over(partition by date(event_datetime),eventName, profile_id order by profile_id,eventName, event_datetime) as row_num
+FROM `bi-staging-1-309112.wowzi_dbt_prod.clevertap_stg_events` 
+  where eventName in ('LoginMethod','LoginWithApple', 'LoginWithFacebook', 'LoginWithGoogle')
+  and profile_id is not null)
+
+SELECT
+    event_datetime,
+    eventName,
+    profile_id,
+    profile_name,
+    profile_email,
+    profile_all_identities,
+    profile_platform,
+    push_token,
+    device_make,
+    device_model,
+    device_os_version,
+    app_version,
+    login_type,
+    login_session_id
+FROM app_installs
+UNION ALL 
+SELECT
+    event_datetime,
+    eventName,
+    profile_id,
+    profile_name,
+    profile_email,
+    profile_all_identities,
+    profile_platform,
+    push_token,
+    device_make,
+    device_model,
+    device_os_version,
+    app_version,
+    login_type,
+    login_session_id
+FROM logins
+    WHERE row_num = 1
