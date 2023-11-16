40c035ae438fad482abc248758b812024e2f2635
mateo2181 <mateomerlo@gmail.com>
2023-11-16 07:14:42 -0300

    Updated cube models.

---
+++ models/marts/cube/campaign_post_platform_metrics.sql
@@ -0,0 +1,48 @@
+{{ config(tags=["cube"]) }}
+
+SELECT
+  tweets.task_id,
+  upper(channel) as channel,
+  campaign_id,
+  influencer_id,
+  influencer_name,
+  followers_count as influencer_followers,
+  submission_link,
+  likes,
+  NULL AS views,
+  retweet_count as shares,
+  reply_count as comments,
+  quote_count as quotes,
+  impressions,
+  TIMESTAMP_TRUNC(processed_at, SECOND) AS updated_at
+FROM
+  {{ref('tweets_insights')}} tweets
+INNER JOIN (
+  SELECT
+    task_id,
+    MAX(processed_at) AS mx_date
+  FROM
+     {{ref('tweets_insights')}}
+  GROUP BY
+    task_id ) ms
+ON
+  ms.task_id=tweets.task_id and
+  ms.mx_date=processed_at
+UNION DISTINCT
+SELECT
+  task_id,
+  upper(channel) as channel,
+  campaign_id,
+  influencer_id,
+  influencer_name,
+  followers_count as influencer_followers,
+  submission_link,
+  likes,
+  views,
+  shares,
+  comments,
+  NULL as quotes,
+  NULL as impressions,
+  TIMESTAMP(updated_at) AS updated_at
+FROM
+  {{ ref('stg__manual_metrics') }}
--- models/marts/cube/tweets_insights.sql
+++ models/marts/cube/tweets_insights.sql
@@ -5,6 +5,7 @@ SELECT
     twitter_data.influencer_id,
     influencers.first_name,
     influencers.last_name,
+    concat(influencers.first_name, " ", influencers.last_name) as influencer_name,
     twitter_data.task_id,
     tasks.submission_status,
     tasks.first_verification_status,
@@ -36,4 +37,4 @@ FROM
 LEFT JOIN {{ ref('postgres_stg__influencers') }} influencers on twitter_data.influencer_id = influencers.influencer_id
 LEFT JOIN {{ ref('postgres_stg__influencer_tasks') }} tasks on twitter_data.task_id = tasks.task_id
 WHERE
-   twitter_data.error='' and twitter_data.tweet_text is not null and twitter_data.username is not null
+    twitter_data.error='' and twitter_data.tweet_text is not null and twitter_data.username is not null
