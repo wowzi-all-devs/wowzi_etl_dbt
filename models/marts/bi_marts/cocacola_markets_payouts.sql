d97482c7f57ff4089861a2255da4130ed0c4714d
kanataki-zwei <115462648+kanataki-zwei@users.noreply.github.com>
2024-03-05 16:02:13 +0300

    adding model for calculating cumulative balances, extracting brand names from on platform campaigns

--- models/marts/bi_marts/cocacola_markets_payouts.sql
+++ models/marts/bi_marts/cocacola_markets_payouts.sql
@@ -9,26 +9,42 @@ WITH platform_payments AS
   campaign_id,
   task_id,
   campaign_name,
+  lower(REGEXP_REPLACE(campaign_name, r'[^a-zA-Z0-9]', '')) clean_campaign_name,
   social_media_channel,
   task_influencer_type,
   payment_date,
-  country,
+  INITCAP(c.country) country,
   amount,
   amount_usd,
   currency,
   payment_status,
   bank_name,
   company_name,
+  'Coca Cola' as client,
+  CASE 
+    WHEN REGEXP_CONTAINS(lower(campaign_name), r'\b(coke|sprite|keringet|cappy|minute maid|fanta|schweppes)\b') 
+    THEN INITCAP(REGEXP_EXTRACT(lower(campaign_name), r'\b(coke|sprite|keringet|cappy|minute maid|fanta|schweppes)\b'))
+    WHEN REGEXP_CONTAINS(lower(campaign_name), r'\b(cokewithmeals)\b') 
+    THEN 'Coke'
+    WHEN REGEXP_CONTAINS(lower(campaign_name), r'\b(coca-cola)\b') 
+    THEN 'Coke'
+    WHEN REGEXP_CONTAINS(lower(campaign_name), r'\b(coca cola)\b') 
+    THEN 'Coke'
+    ELSE NULL
+  END AS brand, 
   reference,
   narration,
   'Platform' as datasource
-FROM `bi-staging-1-309112.wowzi_dbt_prod.influencer_payouts` 
+FROM `bi-staging-1-309112.wowzi_dbt_prod.influencer_payouts` p
+left join bi-staging-1-309112.wowzi_dbt_prod.country_key c
+on (lower(p.country) = lower(c.Key))
   where lower(REGEXP_REPLACE(company_name, r'[^a-zA-Z0-9]', '')) like '%cocacola%'),
-
+/*
 max_transfer_id as 
 (SELECT  
   max(transfer_id) + 1000 max_transfer_id
 FROM `bi-staging-1-309112.wowzi_dbt_prod.influencer_payouts` ),
+*/
 
 periphery_payments AS
 (SELECT  
@@ -51,12 +67,15 @@ periphery_payments AS
     payment_status,
     bank bank_name,
     null company_name,
+    client,
+    brand,
     null reference,
     null narration,
     'Periphery Sheet' as datasource
-FROM `bi-staging-1-309112.wowzi_dbt_prod.periphery_markets_data_clean`)
+FROM `bi-staging-1-309112.wowzi_dbt_prod.periphery_markets_data_clean`),
 
-select 
+all_payments as
+(select 
     transfer_id,
     influencer_id,
     influencer_name,
@@ -76,6 +95,9 @@ select
     else 'Pending'
     end payment_status,
     bank_name,
+    company_name,
+    client,
+    brand,
     datasource
 from platform_payments
 union all 
@@ -96,5 +118,34 @@ select
     currency,
     payment_status,
     bank_name,
+    null company_name,
+    client,
+    brand,
     datasource
-from periphery_payments
+from periphery_payments)
+
+
+SELECT 
+    a.transfer_id,
+    a.influencer_id,
+    a.influencer_name,
+    a.gender,
+    a.campaign_id,
+    a.task_id,
+    a.campaign_name,
+    a.social_media_channel,
+    a.task_influencer_type,
+    a.payment_date,
+    concat(FORMAT_DATETIME("%b", DATETIME(date(payment_date))),"-", extract(year from date(payment_date))) mon_yr,
+    dense_rank () over (order by extract(year from payment_date) asc, extract(month from payment_date)asc ) mon_yr_rnk,
+    a.country,
+    a.amount,
+    a.amount_usd,
+    a.currency,
+    a.payment_status,
+    a.bank_name,
+    a.company_name,
+    a.client,
+    a.brand,
+    a.datasource
+FROM all_payments a 
---
+++ models/marts/bi_marts/cocacola_simplyblack_wallet_balances.sql
@@ -0,0 +1,101 @@
+With campaign_payments as
+(SELECT 
+    DATE_TRUNC(a.payment_date, MONTH) month,
+    concat(FORMAT_DATETIME("%b", DATETIME(date(a.payment_date))),"-", extract(year from date(a.payment_date))) mon_yr,
+    dense_rank () over (order by extract(year from a.payment_date) asc, extract(month from a.payment_date)asc ) mon_yr_rnk,
+    a.campaign_name,
+    a.company_name,
+    a.client,
+    a.brand,
+    a.datasource,
+    a.country,
+    a.amount,
+    a.amount_usd
+FROM `bi-staging-1-309112.wowzi_dbt_prod.cocacola_markets_payouts` a
+    where a.payment_status = 'Successful'),
+
+wallet_amount as 
+(SELECT 
+  'Simply Black' agency,
+  sum(amount_usd) amount_usd
+FROM `bi-staging-1-309112.wowzi_dbt_prod.simply_black_cashflow` ),
+
+aggregate_campaign_payments as
+(select 
+    c.month,
+    c.mon_yr,
+    c.mon_yr_rnk,
+    c.campaign_name,
+    c.company_name,
+    c.client,
+    c.brand,
+    c.datasource,
+    c.country,
+    sum(c.amount) amount,
+    sum(c.amount_usd) amount_usd,
+    w.amount_usd wallet_amount,
+    row_number() over(order by month,  country ) rnk
+from campaign_payments c 
+left join wallet_amount w 
+on w.agency = w.agency
+and w.amount_usd = w.Amount_USD
+    group by c.month,c.mon_yr,c.mon_yr_rnk,c.campaign_name,c.company_name,c.client,c.brand,c.datasource,c.country, w.amount_usd
+    order by month, country),
+
+aggregate_campaign_payments_with_wallet_bal as
+(select 
+    c.month,
+    c.mon_yr,
+    c.mon_yr_rnk,
+    c.campaign_name,
+    c.company_name,
+    c.client,
+    c.brand,
+    c.datasource,
+    c.country,
+    c.amount,
+    c.amount_usd,
+    --c.wallet_amount,
+    c.rnk,
+    case when rnk = 1 then wallet_amount else 0 end as wallet_amt
+from aggregate_campaign_payments c),
+
+final_table as
+(select 
+    month,
+    mon_yr,
+    mon_yr_rnk,
+    campaign_name,
+    company_name,
+    client,
+    brand,
+    datasource,
+    country,
+    amount,
+    amount_usd,
+    rnk,
+    wallet_amt,
+    wallet_amt-amount_usd balance
+from aggregate_campaign_payments_with_wallet_bal
+    order by rnk)
+
+
+select 
+    month,
+    mon_yr,
+    mon_yr_rnk,
+    campaign_name,
+    company_name,
+    client,
+    brand,
+    datasource,
+    country,
+    amount,
+    rnk,
+    wallet_amt,
+    amount_usd,
+    balance,
+    sum(balance) over(order by month, country desc 
+    ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) wallet_balance
+from final_table
+    order by rnk
