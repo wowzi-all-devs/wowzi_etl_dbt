bbecd7cc0db7bcc47737897357749f56c462bd90
kanataki-zwei <115462648+kanataki-zwei@users.noreply.github.com>
2024-05-30 18:15:44 +0300

    changed the model to payable amounts instead of paid amounts

--- models/marts/bi_marts/influencer_jobvalue_brackets.sql
+++ models/marts/bi_marts/influencer_jobvalue_brackets.sql
@@ -10,16 +10,22 @@ WITH job_breakdwon AS
     amount_usd,
     case
     when 
-    amount_usd > 0 and amount_usd <= 100 then '$100 and Below'
+    payment_amount_list_usd <= 50 then '$50 and Below'
     when 
-    amount_usd >= 101 and amount_usd <= 500 then '$101 - $500'
+    payment_amount_list_usd > 50  and payment_amount_list_usd <= 100 then '$51 - $100'
     when 
-    amount_usd >= 501 and amount_usd <= 1000 then '$501 - $1000'
+    payment_amount_list_usd >= 101 and payment_amount_list_usd <= 500 then '$101 - $500'
     when 
-    amount_usd > 1000  then 'More than $1000' else 'Uncategorized' 
+    payment_amount_list_usd >= 501 and payment_amount_list_usd <= 1000 then '$501 - $1000'
+    when 
+    payment_amount_list_usd > 1000 and payment_amount_list_usd <= 10000 then '$1001 - $10000' 
+    when 
+    payment_amount_list_usd >= 10000 then 'More than $10000' 
+    else 'Uncategorized' 
     end as Income_bucket,
     datasource
-FROM `bi-staging-1-309112.wowzi_dbt_prod.influencer_job_breakdown`)
+FROM `bi-staging-1-309112.wowzi_dbt_prod.influencer_job_breakdown`
+where lower(job_status) in ('complete', 'ongoing'))
 
 select 
     influencer_id,
