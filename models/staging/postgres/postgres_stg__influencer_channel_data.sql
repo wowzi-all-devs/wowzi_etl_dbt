a3f9aa4e0f27bf6e77d3e24fc9879bff16fe33cc
kanataki-zwei <kanataki-zwei@users.noreply.github.com>
2024-06-03 06:32:36 +0000

    Squashed commit of the following
    commit 037689f6 
    Author: kanataki-zwei <115462648+kanataki-zwei@users.noreply.github.com> 
    Date: Mon Jun 03 2024 09:29:47 GMT+0300 (East Africa Time) 
    
        adding clean usernames from instagram links

--- models/staging/postgres/postgres_stg__influencer_channel_data.sql
+++ models/staging/postgres/postgres_stg__influencer_channel_data.sql
@@ -3,7 +3,27 @@ SELECT
     channel,
     influencer_id,
     cast(followers_count as INT) as followers_count ,
-    username,
+    CASE
+        WHEN STARTS_WITH(username, 'https://www.instagram.com/') THEN 
+            SUBSTR(
+                username,
+                LENGTH('https://www.instagram.com/') + 1,
+                STRPOS(SUBSTR(username, LENGTH('https://www.instagram.com/') + 1), '/') - 1
+            )
+        WHEN STARTS_WITH(username, 'https://instagram.com/') THEN 
+            SUBSTR(
+                username,
+                LENGTH('https://instagram.com/') + 1,
+                CASE 
+                    WHEN STRPOS(username, '?igshid=') > 0 THEN
+                        STRPOS(username, '?igshid=') - LENGTH('https://instagram.com/') - 1
+                    ELSE
+                        LENGTH(username) - LENGTH('https://instagram.com/')
+                END
+            )
+        ELSE
+            username
+    END AS username,
     status,
     status_backofficer_id,
     status_comment,
