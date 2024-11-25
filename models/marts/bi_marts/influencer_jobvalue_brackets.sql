9daef8ea73fc765e895034ff026fc8a90263e8d0
patrick <patrick@patricks-MacBook-Air.local>
2024-11-25 17:16:41 +0300

    adding gender columns to all jobvalue models

--- models/marts/bi_marts/influencer_jobvalue_brackets.sql
+++ models/marts/bi_marts/influencer_jobvalue_brackets.sql
@@ -1,64 +1,64 @@
-WITH job_breakdwon AS
-(SELECT 
+WITH job_breakdown AS
+(
+SELECT 
     influencer_id,
+    gender,
     country,
-    extract(month from job_offer_date) mon_no,
+    extract(MONTH FROM job_offer_date) mon_no,
     FORMAT_DATETIME("%b", DATETIME(date(job_offer_date))) mon,
-    extract(year from job_offer_date) yr,
     mon_yr,
-    dense_rank () over (order by extract(year from job_offer_date) asc, extract(month from job_offer_date)asc ) mon_yr_rnk,
-    influencer_level,
+    mon_yr_rnk,
     amount_usd,
-    payment_amount_list_usd,
-    datasource
+    payment_amount_list_usd
 FROM `bi-staging-1-309112.wowzi_dbt_prod.influencer_job_breakdown`
-where lower(job_status) in ('complete', 'ongoing')),
+    WHERE LOWER(job_status) IN ('complete', 'ongoing')
+),
 
 aggregates AS
 (
-select 
+SELECT 
     influencer_id,
+    gender,
     country,
     mon_no,
     mon,
-    yr,
     mon_yr,
     mon_yr_rnk,
     sum(amount_usd) amount_usd,
     sum(payment_amount_list_usd) payment_amount_list_usd
-from job_breakdwon
-    group by influencer_id,
+FROM job_breakdown
+    GROUP BY influencer_id,
+    gender,
     country,
     mon_no,
     mon,
-    yr,
     mon_yr,
     mon_yr_rnk
 )
 
-select 
+SELECT 
     influencer_id,
+    gender,
     country,
     mon_no,
     mon,
-    yr,
     mon_yr,
     mon_yr_rnk,
     amount_usd,
     payment_amount_list_usd,
-    case
-    when 
-    round(payment_amount_list_usd,0) <= 50 then '$50 and Below'
-    when 
-    round(payment_amount_list_usd,0) > 50  and round(payment_amount_list_usd,0) <= 100 then '$51 - $100'
-    when 
-    round(payment_amount_list_usd,0) >= 101 and round(payment_amount_list_usd,0) <= 500 then '$101 - $500'
-    when 
-    round(payment_amount_list_usd,0) >= 501 and round(payment_amount_list_usd,0) <= 1000 then '$501 - $1000'
-    when 
-    round(payment_amount_list_usd,0) > 1000 and round(payment_amount_list_usd,0) <= 10000 then '$1001 - $10000' 
-    when 
-    round(payment_amount_list_usd,0) >= 10000 then 'More than $10000' 
-    else 'Uncategorized' 
-    end as Income_bucket
-from job_breakdwon
+    CASE
+        WHEN round(payment_amount_list_usd,0) <= 50 
+            THEN '$50 and Below'
+        WHEN round(payment_amount_list_usd,0) > 50  AND round(payment_amount_list_usd,0) <= 100 
+            THEN '$51 - $100'
+        WHEN round(payment_amount_list_usd,0) >= 101 AND round(payment_amount_list_usd,0) <= 500 
+            THEN '$101 - $500'
+        WHEN round(payment_amount_list_usd,0) >= 501 and round(payment_amount_list_usd,0) <= 1000 
+            THEN '$501 - $1000'
+        WHEN round(payment_amount_list_usd,0) > 1000 and round(payment_amount_list_usd,0) <= 10000 
+            THEN '$1001 - $10000' 
+        WHEN round(payment_amount_list_usd,0) >= 10000 
+            THEN 'More than $10000' 
+        else 'Uncategorized' 
+    END AS Income_bucket
+FROM job_breakdown
--- models/marts/bi_marts/influencer_jobvalue_quarterly_brackets.sql
+++ models/marts/bi_marts/influencer_jobvalue_quarterly_brackets.sql
@@ -1,23 +1,28 @@
-WITH job_breakdwon AS
-(SELECT 
+WITH job_breakdown AS
+(
+SELECT 
     influencer_id,
+    gender,
     country,
-    extract(year from job_offer_date) yr,
+    extract(YEAR FROM job_offer_date) yr,
     CONCAT(
     CAST(EXTRACT(YEAR FROM date(job_offer_date)) AS STRING),
     ' Q', EXTRACT(QUARTER FROM date(job_offer_date))) AS quarter,
     extract(quarter from job_offer_date) quarter_no,
-    dense_rank () over (order by extract(year from job_offer_date) asc, extract(quarter from job_offer_date)asc ) qtr_yr_rnk,
+    dense_rank () OVER (ORDER BY extract(YEAR FROM job_offer_date) ASC, extract(QUARTER FROM job_offer_date)ASC ) qtr_yr_rnk,
     influencer_level,
     amount_usd,
     payment_amount_list_usd,
     datasource
 FROM `bi-staging-1-309112.wowzi_dbt_prod.influencer_job_breakdown`
-where lower(job_status) in ('complete', 'ongoing')),
+    WHERE lower(job_status) IN ('complete', 'ongoing')
+),
 
 quarter_aggregates AS
-(select 
+(
+SELECT 
     influencer_id,
+    gender,
     country,
     yr,
     quarter,
@@ -25,12 +30,13 @@ quarter_aggregates AS
     qtr_yr_rnk,
     sum(amount_usd) amount_usd,
     sum(payment_amount_list_usd) payment_amount_list_usd
-from job_breakdwon
-    group by influencer_id, country, yr, quarter, quarter_no, qtr_yr_rnk
+FROM job_breakdown
+    GROUP BY influencer_id, gender, country, yr, quarter, quarter_no, qtr_yr_rnk
 )
 
 SELECT
     influencer_id,
+    gender,
     country,
     yr,
     quarter,
@@ -38,19 +44,19 @@ SELECT
     qtr_yr_rnk,
     amount_usd,
     payment_amount_list_usd,
-    case
-    when 
-    round(payment_amount_list_usd,0) <= 50 then '$50 and Below'
-    when 
-    round(payment_amount_list_usd,0) > 50  and round(payment_amount_list_usd,0) <= 100 then '$51 - $100'
-    when 
-    round(payment_amount_list_usd,0) >= 101 and round(payment_amount_list_usd,0) <= 500 then '$101 - $500'
-    when 
-    round(payment_amount_list_usd,0) >= 501 and round(payment_amount_list_usd,0) <= 1000 then '$501 - $1000'
-    when 
-    round(payment_amount_list_usd,0) > 1000 and round(payment_amount_list_usd,0) <= 10000 then '$1001 - $10000' 
-    when 
-    round(payment_amount_list_usd,0) >= 10000 then 'More than $10000' 
-    else 'Uncategorized' 
-    end as Income_bucket
+    CASE
+        WHEN round(payment_amount_list_usd,0) <= 50 
+            THEN '$50 and Below'
+        WHEN round(payment_amount_list_usd,0) > 50  AND round(payment_amount_list_usd,0) <= 100 
+            THEN '$51 - $100'
+        WHEN round(payment_amount_list_usd,0) >= 101 AND round(payment_amount_list_usd,0) <= 500 
+            THEN '$101 - $500'
+        WHEN round(payment_amount_list_usd,0) >= 501 AND round(payment_amount_list_usd,0) <= 1000 
+            THEN '$501 - $1000'
+        WHEN round(payment_amount_list_usd,0) > 1000 AND round(payment_amount_list_usd,0) <= 10000 
+            THEN '$1001 - $10000' 
+        WHEN round(payment_amount_list_usd,0) >= 10000 
+            THEN 'More than $10000' 
+        ELSE 'Uncategorized' 
+    END AS Income_bucket
 FROM quarter_aggregates
