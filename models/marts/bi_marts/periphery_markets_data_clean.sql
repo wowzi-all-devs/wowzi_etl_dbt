40afd28ff9cd04891065df32ecd5d337ed6b5cc2
patrick <patrick@patricks-MacBook-Air.local>
2025-01-06 12:46:38 +0300

    adding different source file for gsheet based tables

--- models/marts/bi_marts/periphery_markets_data_clean.sql
+++ models/marts/bi_marts/periphery_markets_data_clean.sql
@@ -1,92 +1,114 @@
 WITH max_starters AS 
-(SELECT 
-  max(job_id) + 1000 as job_id_start,
-  max(campaign_id) + 1000 as campaign_id_start,
-  max(company_id) + 1000 as company_id_start,
-  max(task_id) + 1000 as task_id_start,
-  max(influencer_id) + 1000 as influencer_id_start
-FROM `bi-staging-1-309112.wowzi_dbt_prod.influencer_task_facts` ),
+(
+SELECT 
+  max(job_id) + 1000 AS job_id_start,
+  max(campaign_id) + 1000 AS campaign_id_start,
+  max(company_id) + 1000 AS company_id_start,
+  max(task_id) + 1000 AS task_id_start,
+  max(influencer_id) + 1000 AS influencer_id_start
+FROM {{ ref('influencer_task_facts') }} 
+),
 
 inf_id_setup AS 
-(select
+(
+SELECT
   influencer,
   country,
-  row_number() over(order by country,influencer) as id
-from
-(SELECT 
-  distinct
+  row_number() OVER(ORDER BY country,influencer) AS id
+FROM
+(
+SELECT 
+  DISTINCT
   influencer_name influencer,
   country
-FROM `bi-staging-1-309112.wowzi_dbt_prod.periphery_markets_data` )
-order by country,influencer),
+FROM {{ source('staging', 'periphery_markets_data') }} 
+)
+  ORDER BY country,influencer
+),
 
-final_influencer_ids as
-(Select 
+final_influencer_ids AS
+(
+SELECT 
     i.influencer,
     i.country,
-    m.influencer_id_start + i.id as influencer_id
-from inf_id_setup i 
-left join max_starters m on m.influencer_id_start = m.influencer_id_start),
+    m.influencer_id_start + i.id AS influencer_id
+FROM inf_id_setup i 
+LEFT JOIN max_starters m ON m.influencer_id_start = m.influencer_id_start
+),
 
 campaign_id_setup AS 
-(select
+(
+SELECT
   campaign_name,
   country,
-  row_number() over(order by campaign_name, country) as id
-from
-(SELECT 
-  distinct
+  row_number() OVER(ORDER BY campaign_name, country) AS id
+FROM
+(
+SELECT 
+  DISTINCT
   campaign_name,
   country,
-FROM `bi-staging-1-309112.wowzi_dbt_prod.periphery_markets_data` ) a
-order by campaign_name, country),
+FROM {{ source('staging', 'periphery_markets_data') }} 
+) a
+  ORDER BY campaign_name, country
+),
 
-final_campaign_ids as
-(Select 
+final_campaign_ids AS
+(
+SELECT 
     i.campaign_name,
     i.country,
-    m.campaign_id_start + i.id as campaign_id
-from campaign_id_setup i 
-left join max_starters m on m.campaign_id_start = m.campaign_id_start),
+    m.campaign_id_start + i.id AS campaign_id
+FROM campaign_id_setup i 
+LEFT JOIN max_starters m ON m.campaign_id_start = m.campaign_id_start
+),
 
-job_tasks_id_setup as 
-(SELECT 
+job_tasks_id_setup AS 
+(
+SELECT 
   country,
   campaign_name,
   influencer_name influencer,
-  row_number() over() as id
-FROM `bi-staging-1-309112.wowzi_dbt_prod.periphery_markets_data` ),
+  row_number() OVER() AS id
+FROM {{ source('staging', 'periphery_markets_data') }} 
+),
 
-final_job_tasks_ids as
-(Select 
+final_job_tasks_ids AS
+(
+SELECT 
     i.campaign_name,
     i.country,
     i.influencer,
-    m.job_id_start + i.id as job_id,
-    m.task_id_start + i.id as task_id,
-    m.company_id_start as company_id
-from job_tasks_id_setup i 
-left join max_starters m on m.job_id_start = m.job_id_start),
+    m.job_id_start + i.id AS job_id,
+    m.task_id_start + i.id AS task_id,
+    m.company_id_start AS company_id
+FROM job_tasks_id_setup i 
+LEFT JOIN max_starters m ON m.job_id_start = m.job_id_start
+),
 
-first_dates as 
-(SELECT 
+first_dates AS 
+(
+SELECT 
   country,
   influencer_name influencer,
-  min(payment_date) as first_date
-FROM `bi-staging-1-309112.wowzi_dbt_prod.periphery_markets_data` 
-group by country,influencer_name),
+  min(payment_date) AS first_date
+FROM {{ source('staging', 'periphery_markets_data') }} 
+  GROUP BY country,influencer_name
+),
 
-last_dates as 
-(SELECT 
+last_dates AS 
+(
+SELECT 
   country,
   influencer_name influencer,
-  max(payment_date) as last_date
-FROM `bi-staging-1-309112.wowzi_dbt_prod.periphery_markets_data` 
-group by country,influencer_name),
+  max(payment_date) AS last_date
+FROM {{ source('staging', 'periphery_markets_data') }}
+  GROUP BY country,influencer_name
+),
 
 final_output AS
 (
-select 
+SELECT 
   p.campaign_date,
   p.payment_date,
   p.country,
@@ -95,23 +117,31 @@ select
   p.brand,
   c.campaign_id,
   p.campaign_name,
-  null temp_camp_name,
-  case when p.social_media_platform is null then 'INSTAGRAM'
-  else upper(p.social_media_platform) end social_media_platform,
+  NULL temp_camp_name,
+  CASE 
+    WHEN p.social_media_platform IS NULL THEN 'INSTAGRAM'
+    ELSE upper(p.social_media_platform) 
+  END social_media_platform,
   f.influencer_id,
   fd.first_date inf_date_account_created,
   fd.first_date first_campaign_date,
   ld.last_date last_campaign_date,
   p.influencer_name influencer,
-  case when p.age is null then 26
-  else cast(p.age as int) end age,
-  case when p.age is null then '26-35'
-  when cast(p.age as int) >= 18 and cast(p.age as int) <= 25 then '18-25'
-  when cast(p.age as int) >= 26 and cast(p.age as int) <= 35 then '26-35'
-  when cast(p.age as int) >= 36 and cast(p.age as int) <= 45 then '36-45'
-  else '>45' end as inf_age_range,
-  case when p.gender is null then 'Male'
-  else p.gender end gender,
+  case 
+    when p.age is null then 26
+    else cast(p.age as int) 
+  end age,
+  case 
+    when p.age is null then '26-35'
+    when cast(p.age as int) >= 18 and cast(p.age as int) <= 25 then '18-25'
+    when cast(p.age as int) >= 26 and cast(p.age as int) <= 35 then '26-35'
+    when cast(p.age as int) >= 36 and cast(p.age as int) <= 45 then '36-45'
+    else '>45' 
+  end as inf_age_range,
+  case 
+    when p.gender is null then 'Male'
+    else p.gender 
+  end gender,
   case when p.influencer_level is null then 'Macro'
   else p.influencer_level end influencer_level,
   j.company_id,
@@ -124,7 +154,7 @@ select
   COALESCE(p.amount_lcy, 0.0) amount_lcy,
   COALESCE(p.amount_usd, 0.0) amount_usd,
   p.payment_status
-from `bi-staging-1-309112.wowzi_dbt_prod.periphery_markets_data` p
+from {{ source('staging', 'periphery_markets_data') }} p
 left join final_campaign_ids c on p.campaign_name = c.campaign_name
 and p.country = c.country
 left join final_influencer_ids f on p.influencer_name = f.influencer
--- models/marts/people/team_leads_one_on_ones.sql
+++ models/marts/people/team_leads_one_on_ones.sql
@@ -3,5 +3,6 @@ SELECT
   Team team_name,
   Team_Leader team_lead,
   Team_Member team_member
-FROM `bi-staging-1-309112.wowzi_dbt_prod.one_on_one_tracker` 
+FROM {{ source('staging', 'one_on_one_tracker') }} 
+--`bi-staging-1-309112.wowzi_dbt_prod.one_on_one_tracker` 
     WHERE Date is not null
