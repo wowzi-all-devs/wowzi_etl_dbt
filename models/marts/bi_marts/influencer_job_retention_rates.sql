b1c3315b8a79b40e9c0f9abf537ec76d29785235
patrick <patrick@patricks-MacBook-Air.local>
2025-01-27 13:06:19 +0300

    adding active month data point on retention model

--- models/marts/bi_marts/influencer_job_retention_rates.sql
+++ models/marts/bi_marts/influencer_job_retention_rates.sql
@@ -1,60 +1,96 @@
-with cohort_items as 
-(select
-    min(cohort_month) cohort_month,
+WITH cohort_items as 
+(
+SELECT
+    MIN(cohort_month) cohort_month,
   influencer_id
-from
-(SELECT  
+FROM
+(
+SELECT  
   influencer_id,
-  cast(date_trunc(date(job_offer_date), month) as date) cohort_month
+  CAST(date_trunc(DATE(job_offer_date), MONTH) AS DATE) cohort_month
 FROM `bi-staging-1-309112.wowzi_dbt_prod.influencer_job_breakdown` 
-  where influencer_id is not null
-  and job_status in ('Complete', 'Ongoing')) 
-  group by influencer_id
-  order by cohort_month, influencer_id),
+  WHERE influencer_id IS NOT NULL
+  AND job_status IN ('Complete', 'Ongoing')
+) 
+  GROUP BY influencer_id
+  ORDER BY cohort_month, influencer_id
+),
 
-influencer_activities as 
-(select
-  distinct 
+influencer_activities AS 
+(
+SELECT
+  DISTINCT 
   a.influencer_id,
   a.job_offer_month,
-  date_diff(a.job_offer_month, b.cohort_month, month) month_number
-from
-(select 
+  DATE_DIFF(a.job_offer_month, b.cohort_month, MONTH) month_number
+FROM
+(
+SELECT 
     a.influencer_id,
-    date_trunc(date(job_offer_date), month) job_offer_month
-from bi-staging-1-309112.wowzi_dbt_prod.influencer_job_breakdown a
-  where influencer_id is not null
-  and job_status in ('Complete', 'Ongoing')) a
-left join cohort_items b on a.influencer_id = b.influencer_id),
+    date_trunc(DATE(job_offer_date), MONTH) job_offer_month
+FROM bi-staging-1-309112.wowzi_dbt_prod.influencer_job_breakdown a
+  WHERE influencer_id IS NOT NULL
+  AND job_status IN ('Complete', 'Ongoing')
+) a
+LEFT JOIN cohort_items b ON a.influencer_id = b.influencer_id
+),
 
-cohort_size as 
-(select 
+cohort_size AS 
+(
+SELECT 
     a.cohort_month,
-    count(a.influencer_id) num_influencers
-from cohort_items a
-    group by cohort_month
-    order by cohort_month),
+    COUNT(a.influencer_id) num_influencers
+FROM cohort_items a
+    GROUP BY cohort_month
+    ORDER BY cohort_month
+),
 
-retention_table as
-(select
+retention_table1 AS
+(
+SELECT
     b.cohort_month,
     a.month_number,
     count(a.influencer_id) num_influenccers
-from influencer_activities a 
-left join cohort_items b on a.influencer_id =b.influencer_id
-    group by b.cohort_month, a.month_number)
+FROM influencer_activities a 
+LEFT JOIN cohort_items b ON a.influencer_id =b.influencer_id
+    GROUP BY b.cohort_month, a.month_number
+),
 
-select 
+retention_table2 AS
+(
+SELECT
+    b.cohort_month,
+    a.month_number,
+    a.job_offer_month active_month
+FROM influencer_activities a 
+LEFT JOIN cohort_items b ON a.influencer_id =b.influencer_id
+),
+
+retention_table AS 
+(
+SELECT 
+  a.cohort_month,
+  a.month_number,
+  b.active_month,
+  a.num_influenccers
+FROM retention_table1 a 
+LEFT JOIN retention_table2 b ON a.cohort_month = b.cohort_month
+AND a.month_number = b.month_number
+)
+
+SELECT 
+    DISTINCT
     a.cohort_month,
-    concat( FORMAT_DATETIME("%b", a.cohort_month ),"-", extract(year from a.cohort_month) ) ||' '||'('||b.num_influencers||')' cohort_mon_yr,
-    FORMAT_DATETIME("%b", DATETIME(date(a.cohort_month))) cohort_mon,
-    extract(year from a.cohort_month) cohort_yr,
-    dense_rank () over (order by extract(year from a.cohort_month) asc, extract(month from a.cohort_month)asc ) cohort_yr_rnk,
+    CONCAT( FORMAT_DATETIME("%b", a.cohort_month ),"-", extract(YEAR FROM a.cohort_month) ) ||' '||'('||b.num_influencers||')' cohort_mon_yr,
+    FORMAT_DATETIME("%b", DATETIME(DATE(a.cohort_month))) cohort_mon,
+    EXTRACT(YEAR FROM a.cohort_month) cohort_yr,
+    DENSE_RANK () OVER (ORDER BY extract(YEAR FROM a.cohort_month) ASC, EXTRACT(MONTH FROM a.cohort_month)ASC ) cohort_yr_rnk,
     b.num_influencers total_inf,
     a.month_number,
+    a.active_month,
     a.num_influenccers active_inf,
-    cast(a.num_influenccers as numeric)/b.num_influencers retention_rate
-from retention_table a
-left join cohort_size b on a.cohort_month = b.cohort_month
-    where a.cohort_month is not null
-    order by a.cohort_month, a.month_number
+    CAST(a.num_influenccers AS NUMERIC)/b.num_influencers retention_rate
+FROM retention_table a
+LEFT JOIN cohort_size b ON a.cohort_month = b.cohort_month
+    WHERE a.cohort_month IS NOT NULL
+    ORDER BY a.cohort_month, a.month_number
