23214f03d20fe5ef0b3ba3feec61e16ccf020ded
patrick <patrick@patricks-MacBook-Air.local>
2025-02-10 10:13:13 +0300

    adding sales terms data to the quickbooks sync

---
+++ models/staging/quickbooks/quickbooks_stg__terms.sql
@@ -0,0 +1,10 @@
+SELECT 
+  Id,
+  Name,
+  type,
+  Active,
+  DueDays,
+  --MetaData,
+  JSON_VALUE(MetaData, '$.CreateTime') AS CreateTime,
+  JSON_VALUE(MetaData, '$.LastUpdatedTime') AS LastUpdatedTime
+FROM {{ source('staging', 'terms') }}
