69056a3845ff7d7219e599dbbbfa257b00d6a10e
patrick <patrick@patricks-MacBook-Air.local>
2025-03-12 13:03:47 +0300

    adding rate of change columns

--- models/marts/bi_marts/company_scorecard/company_scorecard_active_creators.sql
+++ models/marts/bi_marts/company_scorecard/company_scorecard_active_creators.sql
@@ -3,6 +3,21 @@ SELECT
   DATE(task_creation_time) as Date,
   influencer_id,
   country,
+  CASE 
+    WHEN DATE(task_creation_time) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+    THEN TRUE 
+    ELSE FALSE 
+  END active_last_24_hours,
+  CASE 
+    WHEN DATE(task_creation_time) >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY)
+    THEN TRUE 
+    ELSE FALSE 
+  END active_last_7_days,
+  CASE 
+    WHEN DATE(task_creation_time) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH)
+    THEN TRUE 
+    ELSE FALSE 
+  END active_last_1_month,
   CAST(NULL AS STRING) function,
   CAST(NULL AS STRING)  dept_name,
   CAST(NULL AS STRING)  metric_name,
@@ -15,6 +30,9 @@ SELECT
   DATE(period) as Date,
   NULL influencer_id,
   NULL country,
+  NULL active_last_24_hours,
+  NULL active_last_7_days,
+  NULL active_last_1_month,
   function,
   dept_name,
   metric_name,
