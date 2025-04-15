84d57953b6a5eeb94438140da5f03a866a8e078a
Kayode Williams <kayodew@Wowzis-MacBook-Air.local>
2025-04-15 22:17:30 +0100

    Added new table fields

--- models/staging/gsheets/gsheets__stg__HEVA_requisition_data.sql
+++ models/staging/gsheets/gsheets__stg__HEVA_requisition_data.sql
@@ -5,6 +5,9 @@ select
     Paid_Out_Date_ payout_date,
     Replenish_Date_ replenish_date,
     Replenished_Amount_ replenished_amount,
+    extract(year from Paid_Out_Date_ ) mon,
+    extract(year from Paid_Out_Date_ ) yr,
+    dense_rank () over (order by extract(year from Paid_Out_Date_ ) asc, extract(month from Paid_Out_Date_ )asc) mon_yr_rnk,
     Balance_ balance
  FROM 
     {{ source('staging', 'gsheet__stg__HEVA_requisition_data')}}
