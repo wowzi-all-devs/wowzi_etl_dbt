e8bf06347d87bec75b67e50ecfc041d246bd0356
Kayode Williams <kayodew@Wowzis-MacBook-Air.local>
2025-04-28 15:50:00 +0100

    Created model for influencer_grouping

---
+++ models/marts/bi_marts/mastercard_training_inf_grouping_metrics.sql
@@ -0,0 +1,40 @@
+with i_groups as
+(
+select id,name
+from 
+{{ source('staging', 'influencer_groups') }} 
+where trim(name) in ('Ajira-Meru', 'Ajira-Mombasa', 'Ajira-Nairobi', 'Ajira Nakuru', 'Sanara-Kisumu',
+                'Sanara-Mombasa', 'Sanara-Nairobi', 'Sanara-Nakuru', 'Sanara-Kakamega',
+                'Sanara-Turkana')
+),
+inf as
+(
+select 
+i_groups.id group_id, 
+i_groups.name group_name, 
+inf.influencer_id, 
+inf.influencer_group_id 
+from {{ source('staging', 'influencer_group_influencers') }} inf
+right join i_groups ------Right join bc my right tbl is where all the groups are that i need matched to inf_id
+on inf.influencer_group_id = i_groups.id 
+),
+inf_data as
+(
+select
+inf.group_id,
+inf.group_name,
+inf.influencer_id, 
+concat(inf_data.first_name,' ', inf_data.last_name) inf_name,
+inf_data.gender,
+inf_data.inf_age_range,
+inf_data.job_eligibility,
+inf_data.job_activity,
+inf_data.clean_country,
+initcap(inf_data.location) location,
+inf_data.completed_one_job
+from inf 
+LEFT JOIN 
+{{ ref('influencer_reg_job_facts_cob') }} inf_data
+ON cast(inf.influencer_id as Int64) = cast(inf_data.influencer_id_a as Int64)
+)
+SELECT * FROM inf_data
