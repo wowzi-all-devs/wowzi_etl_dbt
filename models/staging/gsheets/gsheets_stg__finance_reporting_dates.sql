dedd9c0abe13972ef8a7e277056e85e75828cfaf
Kayode Williams <kayodew@Wowzis-MacBook-Air.local>
2025-05-06 13:29:34 +0100

    Added grace days for reporting

--- models/staging/gsheets/gsheets_stg__finance_reporting_dates.sql
+++ models/staging/gsheets/gsheets_stg__finance_reporting_dates.sql
@@ -2,10 +2,10 @@ SELECT
   Month_Period month_period,
   Year year,
   Month month,
-  Reporting_Due_Date reporting_due_date,
-  Actual_Reporting_Date actual_reporting_date,
-  CASE 
-    WHEN DATE(Actual_Reporting_Date) <= DATE(Reporting_Due_Date) THEN 1 
-    ELSE 0 
+  DATE(Reporting_Due_Date) AS reporting_due_date,
+  DATE(Actual_Reporting_Date) AS actual_reporting_date,
+  CASE
+    WHEN DATE(Actual_Reporting_Date) <= DATE(Reporting_Due_Date) + 2 THEN 1
+    ELSE 0
   END AS target
 FROM {{ source('staging', 'gsheets_finance_reporting_dates') }}
