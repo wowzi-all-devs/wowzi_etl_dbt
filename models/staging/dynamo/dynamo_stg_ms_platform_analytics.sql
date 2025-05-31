43893160b6789eb57f906b1240320eedfcb7fd14
Kayode Williams <kayodew@Wowzis-MacBook-Air.local>
2025-05-31 16:35:16 +0100

    updated platform analytics table

--- models/staging/dynamo/dynamo_stg_ms_platform_analytics.sql
+++ models/staging/dynamo/dynamo_stg_ms_platform_analytics.sql
@@ -1,3 +1,6 @@
+
+with a as 
+(
 select 
   event_id,
   date(created) created,
@@ -7,4 +10,24 @@ select
 --   event_name,
   REGEXP_REPLACE(event_name, r'([a-z])([A-Z])', r'\1 \2') AS fine_event_name -- This regex replaces camelCase with space-separated words
 from 
-  {{ source('staging', 'ms_platform_analytics') }}
+  {{ source('staging', 'ms_platform_analytics') }} 
+),
+country as 
+(
+SELECT * 
+FROM bi-staging-1-309112.dbt_kayode.country_key 
+)
+SELECT 
+  a.event_id,
+  a.created,
+  a.city,
+  country.Country AS country,
+  a.platform,
+  a.fine_event_name
+  from 
+  a LEFT join   
+  country
+  on a.country = country.Key
+
+
+  
