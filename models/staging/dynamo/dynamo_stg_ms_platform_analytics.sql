55275fc8c46fa6e64481d7aa2fadd5f2b6b89473
Kayode Williams <kayodew@Wowzis-MacBook-Air.local>
2025-06-02 09:26:26 +0100

    modified platform anaytics table; unnested user id and organzation

--- models/staging/dynamo/dynamo_stg_ms_platform_analytics.sql
+++ models/staging/dynamo/dynamo_stg_ms_platform_analytics.sql
@@ -3,9 +3,12 @@ with a as
 (
 select 
   event_id,
+  JSON_VALUE(metadata, '$.decoded_ip_info.user_id') AS user_id,
   date(created) created,
-  JSON_VALUE(tracking, '$.decoded_ip_info.city') AS city,
   JSON_VALUE(tracking, '$.decoded_ip_info.country') AS country,
+  JSON_VALUE(tracking, '$.decoded_ip_info.city') AS city,
+  JSON_VALUE(tracking, '$.decoded_ip_info.org') AS org,
+
   platform,
 --   event_name,
   REGEXP_REPLACE(event_name, r'([a-z])([A-Z])', r'\1 \2') AS fine_event_name -- This regex replaces camelCase with space-separated words
@@ -19,11 +22,13 @@ FROM bi-staging-1-309112.dbt_kayode.country_key
 )
 SELECT 
   a.event_id,
+  a.user_id,
   a.created,
   a.city,
   country.Country AS country,
   a.platform,
-  a.fine_event_name
+  a.fine_event_name,
+  REGEXP_REPLACE(a.org, r'^AS\d+\s*', '') AS org
   from 
   a LEFT join   
   country
