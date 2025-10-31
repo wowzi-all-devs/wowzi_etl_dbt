034548b99902ba090e9bae8ac5205980d7f35023
Kayode Williams <kayodew@MACBOOKAIR-6C1D.station>
2025-10-31 00:27:31 +0000

    updated requsition sheet_2

--- models/staging/gsheets/gsheet__prd__HEVA_requsition_data_2.sql
+++ models/staging/gsheets/gsheet__prd__HEVA_requsition_data_2.sql
@@ -1,9 +1,9 @@
 select *, row_number() over (
    partition by Request_No_) as rn,
-       extract(year from paid_out_date_) mon,
-    extract(year from paid_out_date_) yr,
-    concat(FORMAT_DATETIME("%b", DATETIME(date(paid_out_date_))),"-", extract(year from date(paid_out_date_))) mon_yr,
-    dense_rank () over (order by extract(year from paid_out_date_) asc, extract(month from paid_out_date_)asc) mon_yr_rnk,
+       extract(year from request_date_) mon,
+    extract(year from request_date_) yr,
+    concat(FORMAT_DATETIME("%b", DATETIME(date(request_date_))),"-", extract(year from date(request_date_))) mon_yr,
+    dense_rank () over (order by extract(year from request_date_) asc, extract(month from request_date_)asc) mon_yr_rnk,
 from
    {{ source('staging', 'gsheet__stg__HEVA_requsition_data_2')}}
 where Request_No_ is not null
