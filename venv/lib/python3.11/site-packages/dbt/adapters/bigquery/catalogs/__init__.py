3cf9ca6a31fe6ef7c605c67433e22423eb6f083e
Kayode Williams <kayodew@MACBOOKAIR-6C1D.station>
2025-11-20 15:21:08 +0000

    Remove venv directory from repository

--- venv/lib/python3.11/site-packages/dbt/adapters/bigquery/catalogs/__init__.py
+++
@@ -1,3 +0,0 @@
-from dbt.adapters.bigquery.catalogs._biglake_metastore import BigLakeCatalogIntegration
-from dbt.adapters.bigquery.catalogs._info_schema import BigQueryInfoSchemaCatalogIntegration
-from dbt.adapters.bigquery.catalogs._relation import BigQueryCatalogRelation
