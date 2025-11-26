456d37a7f20cfde2810a5ecf55fe09e4a2e62178
Kayode Williams <kayodew@MACBOOKAIR-6C1D.station>
2025-11-26 22:43:40 +0000

    added sentiments

---
+++ models/staging/gsheets/gsheets__stg__safaricom_sentiments_influencers.sql
@@ -0,0 +1,21 @@
+select 
+*,
+-- DATE_TRUNC(date(join_date), week(MONDAY)) =
+--   DATE_SUB(date_trunc(current_date(), week(MONDAY)), INTERVAL 7 DAY)
+--   THEN TRUE
+
+case when 
+    date_trunc(date(week), week(MONDAY)) =
+    date_sub(date_trunc(current_date(), week(MONDAY)), interval 7 day)
+    then TRUE
+    else NULL
+    end AS is_this_week,
+
+case when 
+    date_trunc(date(week), week(MONDAY)) =
+    date_sub(date_trunc(current_date(), week(MONDAY)), interval 14 day)
+    then TRUE
+    else NULL
+    end AS is_last_week
+from
+{{ source('staging', 'gsheets_safaricom_sentiments_influencers')}}
